330cb95d37db0da6e8d93ac28bd73dd7b2179902
Seonghyeon Lee <Seonghyeon.Lee@cognex.com>
2020-05-29 14:42:54 +0900

    Added new project

---
+++ WPF_MVVM_Examples/ProgressBarChildView/ViewModel/ChildViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using MahApps.Metro.Controls;
+using MahApps.Metro.SimpleChildWindow;
+using System.IO;
+using GalaSoft.MvvmLight.Messaging;
+using System.Windows.Threading;
+using ProgressBarChildView.View;
+
+namespace ProgressBarChildView.ViewModel
+{
+    public class ChildViewModel : ViewModelBase
+    {
+        private int _progressMaximum;
+        public int ProgressMaximum
+        {
+            get { return _progressMaximum; }
+            set { _progressMaximum = value; RaisePropertyChanged("ProgressMaximum"); }
+        }
+        private int _progressValue;
+        public int ProgressValue
+        {
+            get { return _progressValue; }
+            set { _progressValue = value; RaisePropertyChanged("ProgressValue"); }
+        }
+
+        public ChildViewModel()
+        {
+
+        }
+    }
+}
---
+++ WPF_MVVM_Examples/ProgressBarChildView/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using MahApps.Metro.Controls;
+using MahApps.Metro.SimpleChildWindow;
+using System.IO;
+
+using ProgressBarChildView.ChildView;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace ProgressBarChildView.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public RelayCommand<object> OnButtonClickCommand { get; private set; }
+
+        public ChildViewModel _ChildViewModel { get; set; }
+        public MainWindowViewModel()
+        {
+            _ChildViewModel = new ChildViewModel();
+            OnButtonClickCommand = new RelayCommand<object>(OnButtonClick);
+        }
+
+        private void OnButtonClick(object sender)
+        {
+            ProgressChildView progressChildView = new ProgressChildView() { DataContext = _ChildViewModel };
+
+            ((MetroWindow)Application.Current.MainWindow).ShowChildWindowAsync(progressChildView);
+
+            _ChildViewModel.ProgressMaximum = 100;
+            Task.Run(() =>
+            {
+                try
+                {
+                    for (int i = 0; i < _ChildViewModel.ProgressMaximum + 1; i++)
+                    {
+                        _ChildViewModel.ProgressValue = i;
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            });
+        }
+    }
+}
